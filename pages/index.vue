d5892afd38625c2ac3195f2738bfcdb4ed385e78
CHEVRIER <simon.chevrier@epitech.eu>
2025-12-02 14:20:14 +0100

    ajout du lien git pour le projet epimon

--- pages/index.vue
+++ pages/index.vue
@@ -97,6 +97,7 @@
       </a>
     </div>
     <div class="bg-white rounded-xl overflow-hidden hover:shadow-xl transition-all duration-300 hover:scale-105 group">
+      <a href="https://github.com/Yougooooo/EPIMON"></a>
       <div class="h-48 bg-slate-200 group-hover:bg-slate-300 transition-colors duration-300">
         <img src="../assets/Epimon.png" alt="image décorative" class="w-full h-full object-cover">
       </div>
